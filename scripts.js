75dddffb2b2ce401d63db664e7fc0bea0ef4d597
z-carlson <zcarlson87@gmail.com>
2020-08-22 17:47:29 -0500

    changes picture categories to animals

--- scripts.js
+++ scripts.js
@@ -8,82 +8,82 @@ const cardData = [
   {
     id: 0,
     text: "",
-    background: "https://source.unsplash.com/1600x900/?forest",
+    background: "https://source.unsplash.com/1600x900/?lion",
   },
   {
     id: 1,
     text: "",
-    background: "https://source.unsplash.com/1600x900/?ocean",
+    background: "https://source.unsplash.com/1600x900/?tiger",
   },
   {
     id: 2,
     text: "",
-    background: "https://source.unsplash.com/1600x900/?snow",
+    background: "https://source.unsplash.com/1600x900/?zebra",
   },
   {
     id: 3,
     text: "",
-    background: "https://source.unsplash.com/1600x900/?grass",
+    background: "https://source.unsplash.com/1600x900/?elephant",
   },
   {
     id: 4,
     text: "",
-    background: "https://source.unsplash.com/1600x900/?desert",
+    background: "https://source.unsplash.com/1600x900/?rhino",
   },
   {
     id: 5,
     text: "",
-    background: "https://source.unsplash.com/1600x900/?mountains",
+    background: "https://source.unsplash.com/1600x900/?bear",
   },
   {
     id: 6,
     text: "",
-    background: "https://source.unsplash.com/1600x900/?tundra",
+    background: "https://source.unsplash.com/1600x900/?shark",
   },
   {
     id: 7,
     text: "",
-    background: "https://source.unsplash.com/1600x900/?swamp",
+    background: "https://source.unsplash.com/1600x900/?deer",
   },
   {
     id: 0,
     text: "",
-    background: "https://source.unsplash.com/1600x900/?forest",
+    background: "https://source.unsplash.com/1600x900/?lion",
   },
   {
     id: 1,
     text: "",
-    background: "https://source.unsplash.com/1600x900/?ocean",
+    background: "https://source.unsplash.com/1600x900/?tiger",
   },
   {
     id: 2,
     text: "",
-    background: "https://source.unsplash.com/1600x900/?snow",
+    background: "https://source.unsplash.com/1600x900/?zebra",
   },
   {
     id: 3,
     text: "",
-    background: "https://source.unsplash.com/1600x900/?grass",
+    background: "https://source.unsplash.com/1600x900/?elephant",
   },
   {
     id: 4,
     text: "",
-    background: "https://source.unsplash.com/1600x900/?desert",
+    background: "https://source.unsplash.com/1600x900/?rhino",
   },
   {
     id: 5,
     text: "",
-    background: "https://source.unsplash.com/1600x900/?mountains",
+    background: "https://source.unsplash.com/1600x900/?bear",
   },
   {
     id: 6,
     text: "",
-    background: "https://source.unsplash.com/1600x900/?tundra",
+    background: "https://source.unsplash.com/1600x900/?shark",
   },
   {
     id: 7,
     text: "",
-    background: "https://source.unsplash.com/1600x900/?swamp",
+    background: "https://source.unsplash.com/1600x900/?deer",
   },
 ];
 
